0e1712c86aa6ff45082fd05cbc968da37b1e6f4e
Jim Nanney <jnanney@gmail.com>
2022-02-04 10:16:13 -0600

    remove failing generated test

--- spec/rubocop/env_spec.rb
+++ spec/rubocop/env_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Rubocop::Env do
   it 'has a version number' do
     expect(Rubocop::Env::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
